bece65f7454aba57efd27a0dbf25060303b6a56f
Bmansell1 <benjaminmansell123456789@gmail.com>
2023-05-19 13:52:22 +0100

    Final version

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 let questionNum = 0;
-let questions = ["Question1", "question2", "question3", "question4"];
-let answers = ["answer 1", "answer 2", "answer 3", "answer 4", "answer 5", "answer 6", "answer 7", "answer 8", "answer 9", "answer 10", "answer 11", "answer 12", "answer 13", "answer 14", "answer 15", "answer 16"];
-let answCor = [1,2,3,4];
+let questions = ["When did the UK decimalise its currency?", "When was the light bulb invented?", "Who was the first man in space?", "What was napoleon's dinnerware made out of?", "What is the capital of the state of new york?"];
+let answers = ["1964", "1971", "1955", "1946", "1879", "1853", "1902", "1814", "Neil Armstrong", "Yuri Gagarin", "Alan Shepard", "Buzz Aldrin", "Gold", "Silver", "Bronze", "Aluminum", "Buffalo", "New York", "Albany", "Utica"];
+let answCor = [2,1,2,4,3];
 let btn = 1;
 let score = 0;
 function nextQuestion(){
@@ -11,16 +11,14 @@ function nextQuestion(){
   document.getElementById("btn2").innerHTML = answers[questionNum*4+1];
   document.getElementById("btn3").innerHTML = answers[questionNum*4+2];
   document.getElementById("btn4").innerHTML = answers[questionNum*4+3];
-  console.log(btn);
   if(answCor[questionNum - 1] == btn){
     score++;
   }
   if(questions.length == questionNum){
-    console.log("worked");
     document.getElementById("Question").style.visibility = "hidden";
     document.getElementById("answers").style.visibility = "hidden";
     document.getElementById("result").style.visibility = "visible";
-    document.getElementById("result").innerHTML = "your score is " + score;
+    document.getElementById("result").innerHTML = "your score is: " + score;
   }
 }
 
